52d2b3b8782871f31771af6059e33f1035d0c0ee
“ming-00” <e0470728@u.nus.edu>
2020-06-02 20:42:01 +0800

    Add Bootstrap and start header layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-  get 'welcome/forum'
-  get 'welcome/feed'
-  get 'welcome/profile'
-  get 'welcome/index'
+  get 'forum'   => 'welcome#forum'
+  get 'feed'    => 'welcome#feed'
+  get 'profile' => 'welcome#profile'
 
-  root 'welcome#index'
+  root             'welcome#index'
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

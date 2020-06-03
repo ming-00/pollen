c7746f3a6ba646dcb942ea93714437c867544c8b
“ming-00” <e0470728@u.nus.edu>
2020-06-03 11:43:29 +0800

    Format sign-in and sign-up form

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get 'feed'    => 'welcome#feed'
   get 'profile' => 'welcome#profile'
 
-  root             'welcome#index'
+  root :to      => 'welcome#index'
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

3945fa06082c5a5c58732362a8206887ef87d866
ming-00 <e0470728@u.nus.edu>
2020-06-05 11:44:32 +0800

    Add sign-up validations and error messages

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
   include Clearance::User
 
   validates :firstname,  presence: true, length: { minimum: 1, maximum: 25 }
-  validates :firstname,  presence: true, length: { minimum: 1, maximum: 25 }
+  validates_format_of :firstname, :with => /\A[a-z]+\z/i
+  validates :lastname,  presence: true, length: { minimum: 1, maximum: 25 }
+  validates_format_of :lastname, :with => /\A[a-z]+\z/i
 
 end

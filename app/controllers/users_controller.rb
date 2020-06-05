4a825667b25686ba06ce1dd136249aab8d0234d5
“ming-00” <e0470728@u.nus.edu>
2020-06-05 11:44:32 +0800

    Add sign-up validations and error messages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,18 @@ class UsersController < Clearance::UsersController
         @user = User.find(params[:id])
     end
 
+    def create
+        @user = user_from_params
+    
+        if @user.save
+          sign_in @user
+          redirect_back_or url_after_create
+        else
+          flash.now[:error] = @user.errors.full_messages[0]
+          render template: "users/new"
+        end
+      end
+
     private
 
     def user_params

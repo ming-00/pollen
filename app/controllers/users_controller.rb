d4186cbda8e980d8f9d001024f71ea29c3852ac1
“ming-00” <e0470728@u.nus.edu>
2020-06-06 12:17:22 +0800

    Add and format restyle messages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,11 +8,18 @@ class UsersController < Clearance::UsersController
         @user = user_from_params
     
         if @user.save
+          flash[:success] = "Welcome to Pollen! Please edit your 
+            information to get started."
           sign_in @user
-          redirect_back_or url_after_create
+          #redirect_back_or url_after_create
+          #above redirects to home page, modified to direct to user instead
+          redirect_to @user
         else
-          flash.now[:error] = @user.errors.full_messages[0]
-          render template: "users/new"
+          #flash.now[:error] = @user.errors.full_messages[0]
+          #render template: "users/new"
+          #changed to below resolve issue regarding formatting
+          flash[:danger] = @user.errors.full_messages[0]
+          redirect_to "/sign_up"
         end
       end
 

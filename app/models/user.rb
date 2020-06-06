33788817ccfae8735931f2d5eae7cfcee2661a7e
ming-00 <e0470728@u.nus.edu>
2020-06-06 12:17:22 +0800

    Add and format restyle messages

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,22 @@
 class User < ApplicationRecord
   include Clearance::User
 
-  validates :firstname,  presence: true, length: { minimum: 1, maximum: 25 }
-  validates_format_of :firstname, :with => /\A[a-z]+\z/i
-  validates :lastname,  presence: true, length: { minimum: 1, maximum: 25 }
-  validates_format_of :lastname, :with => /\A[a-z]+\z/i
+  validates :firstname,  
+    :presence => {:message => "First name can't be blank."},
+    length: { minimum: 1, maximum: 25, 
+      :message => "First name must be between 1 and 25 letters."
+    }
+
+  validates_format_of :firstname, :with => /\A[a-z]+\z/i, 
+    message: "First name must not contain any numbers."
+
+  validates :lastname,  
+    presence: true, 
+    length: { minimum: 1, maximum: 25, 
+      :message => "First name must be between 1 and 25 letters."
+  }
+
+  validates_format_of :lastname, :with => /\A[a-z]+\z/i,
+    message: "Last name must not contain any numbers."
 
 end

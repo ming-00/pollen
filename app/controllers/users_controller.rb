0599da09dfb83a40dced568c0fc9d6fc6bb19723
lihanlilyy <55406206+lihanlilyy@users.noreply.github.com>
2020-06-12 00:35:01 +0800

    Fix bugs in user profile update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < Clearance::UsersController
-    
+  before_action :logged_in_user, only: [:edit, :update]
+  before_action :correct_user,   only: [:edit, :update]
+
     def show
         @user = User.find(params[:id])
     end
@@ -31,7 +33,8 @@ class UsersController < Clearance::UsersController
           #sign_in @user
           redirect_to @user
         else 
-          render 'edit'
+          flash[:danger] = @user.errors.full_messages[0]
+          redirect_to "/sign_up"
         end
       end
 
@@ -49,6 +52,21 @@ class UsersController < Clearance::UsersController
           :firstname, 
           :lastname, 
         )
+    end
+
+     # Confirms a logged-in user.
+     def logged_in_user
+      unless logged_in?
+        store_location
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
+    end
+
+    # Confirms the correct user.
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
     end
 
 

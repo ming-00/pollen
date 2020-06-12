7e706d89399c6561f7bf1088faf53c119ef99975
“ming-00” <e0470728@u.nus.edu>
2020-06-12 16:55:19 +0800

    Fix repeat in flash message

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,22 +2,22 @@ class User < ApplicationRecord
   include Clearance::User
 
   validates :firstname,  
-    :presence => {:message => "First name can't be blank."},
+    :presence => {:message => " can't be blank."},
     length: { minimum: 1, maximum: 25, 
-      :message => "First name must be between 1 and 25 letters."
+      :message => " must be between 1 and 25 letters."
     }
 
   validates_format_of :firstname, :with => /\A[a-z]+\z/i, 
-    message: "First name must not contain any numbers."
+    message: " must not contain any numbers."
 
   validates :lastname,  
     presence: true, 
     length: { minimum: 1, maximum: 25, 
-      :message => "First name must be between 1 and 25 letters."
+      :message => " must be between 1 and 25 letters."
   }
 
   validates_format_of :lastname, :with => /\A[a-z]+\z/i,
-    message: "Last name must not contain any numbers."
+    message: " must not contain any numbers."
 
   module Validations
     extend ActiveSupport::Concern

0e21492e71c1920ec76ac0f7337d93b34ef44a46
lihanlilyy <55406206+lihanlilyy@users.noreply.github.com>
2020-06-21 01:00:07 +0800

    Add actions in ForumpostsController

--- app/controllers/forumposts_controller.rb
+++ app/controllers/forumposts_controller.rb
@@ -1,2 +1,20 @@
 class ForumpostsController < ApplicationController
+    before_action :logged_in_user, only: [:create, :destroy]
+    def create
+        @forumpost = current_user.forumposts.build(forumpost_params)
+        if @forumpost.save
+            flash[:success] = "Post created and published in forum!"
+            redirect_to root_url
+        else
+            render 'welcome/home'
+        end
+    end
+    
+    def destroy
+    end
+
+    private
+    def micropost_params
+        params.require(:forumpost).permit(:content)
+    end
 end

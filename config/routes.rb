aeca567f0a1a89272bfa88e636a7f02370c49d50
lihanlilyy <55406206+lihanlilyy@users.noreply.github.com>
2020-06-21 00:14:40 +0800

    Update controller and add routes

--- config/routes.rb
+++ config/routes.rb
@@ -21,6 +21,7 @@ Rails.application.routes.draw do
   root :to      => 'welcome#index'
   resources :users, only: [:show, :update, :edit]
   resources :journals, only: [:show, :create, :destroy]
+  resources :forumposts, only: [:create, :destroy]
 
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html

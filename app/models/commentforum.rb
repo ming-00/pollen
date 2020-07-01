13fcc862e4cf3ef04d29d7d64e1881266f0f7c43
lihanlilyy <55406206+lihanlilyy@users.noreply.github.com>
2020-07-01 23:04:20 +0800

    Modify comment function in forum

--- app/models/commentforum.rb
+++ app/models/commentforum.rb
@@ -1,4 +1,9 @@
 class Commentforum < ApplicationRecord
   belongs_to :forumpost
   belongs_to :user
+  default_scope -> { order(created_at: :desc) }
+  validates :reply,
+  :presence => {:message => " can't be blank."},
+  length: {minimum: 1, maximum: 2000, 
+    :message => " must be between 1 and 2000 characters."}
 end

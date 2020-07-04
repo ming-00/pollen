c7a543facb7fcebd6e7154d69f31afeffdd5b420
“ming-00” <e0470728@u.nus.edu>
2020-07-04 13:42:04 +0800

    Add default user avatars

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,3 +1,18 @@
 module UsersHelper
-    
+    def avatar_for(user)
+        case (user.id) % 6
+            when 0
+                image_tag 'profile1.png', :class => "avatar"
+            when 1
+                image_tag 'profile2.png', :class => "avatar"
+            when 2
+                image_tag 'profile3.png', :class => "avatar"
+            when 3
+                image_tag 'profile4.png', :class => "avatar"
+            when 4
+                image_tag 'profile5.png', :class => "avatar"
+            else
+                image_tag 'profile6.png', :class => "avatar"
+        end
+    end
 end

31a0238bb93335248590c3ee4dc56c5597cb1d31
“ming-00” <e0470728@u.nus.edu>
2020-07-17 02:13:46 +0800

    Fix view for forumpost

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_14_134828) do
+ActiveRecord::Schema.define(version: 2020_07_15_143629) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"

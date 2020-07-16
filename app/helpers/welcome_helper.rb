6e3056ea1bdde86b6e6680484ddfc6ab029061f4
“ming-00” <e0470728@u.nus.edu>
2020-07-16 21:18:48 +0800

    Customise message according to time

--- app/helpers/welcome_helper.rb
+++ app/helpers/welcome_helper.rb
@@ -1,2 +1,11 @@
 module WelcomeHelper
+    def greet
+        case Time.now.hour
+        when 1..11 then 'Good morning,'
+        when 12..17 then 'Good afternoon,'
+        when 18..24 then 'Good evening,'
+        else
+          'Hello,'
+        end
+    end
 end

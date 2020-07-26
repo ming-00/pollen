dddad35021db5ae2f7c8400949478e170814fd6f
“ming-00” <e0470728@u.nus.edu>
2020-07-26 11:15:49 +0800

    Fix bug in user info

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,13 +2,13 @@ module UsersHelper
     def avatar_for(user)
         case (user.id) % 6
             when 0
-                image_tag 'profile1.png', :class => "avatar"
+                image_tag 'profile4.png', :class => "avatar"
             when 1
                 image_tag 'profile2.png', :class => "avatar"
             when 2
                 image_tag 'profile3.png', :class => "avatar"
             when 3
-                image_tag 'profile4.png', :class => "avatar"
+                image_tag 'profile1.png', :class => "avatar"
             when 4
                 image_tag 'profile5.png', :class => "avatar"
             else
@@ -19,13 +19,13 @@ module UsersHelper
     def tiny_avatar_for(user)
         case (user.id) % 6
             when 0
-                image_tag 'profile1.png', :class => "mini-avatar", data: { toggle: 'tooltip' }, title: "#{user.firstname} #{user.lastname}"
+                image_tag 'profile4.png', :class => "mini-avatar", data: { toggle: 'tooltip' }, title: "#{user.firstname} #{user.lastname}"
             when 1
                 image_tag 'profile2.png', :class => "mini-avatar", data: { toggle: 'tooltip' }, title: "#{user.firstname} #{user.lastname}"
             when 2
                 image_tag 'profile3.png', :class => "mini-avatar", data: { toggle: 'tooltip' }, title: "#{user.firstname} #{user.lastname}"
             when 3
-                image_tag 'profile4.png', :class => "mini-avatar", data: { toggle: 'tooltip' }, title: "#{user.firstname} #{user.lastname}"
+                image_tag 'profile1.png', :class => "mini-avatar", data: { toggle: 'tooltip' }, title: "#{user.firstname} #{user.lastname}"
             when 4
                 image_tag 'profile5.png', :class => "mini-avatar", data: { toggle: 'tooltip' }, title: "#{user.firstname} #{user.lastname}"
             else
@@ -36,13 +36,13 @@ module UsersHelper
     def tiny_blank_avatar_for(user)
         case (user.id) % 6
             when 0
-                image_tag 'profile1.png', :class => "mini-avatar"
+                image_tag 'profile4.png', :class => "mini-avatar"
             when 1
                 image_tag 'profile2.png', :class => "mini-avatar"
             when 2
                 image_tag 'profile3.png', :class => "mini-avatar"
             when 3
-                image_tag 'profile4.png', :class => "mini-avatar"
+                image_tag 'profile1.png', :class => "mini-avatar"
             when 4
                 image_tag 'profile5.png', :class => "mini-avatar"
             else
